87312516599690ec71dc9d768e1776a8452e0860
Mirko Justiniano <mirkoj@gmail.com>
2018-06-21 16:03:17 -0500

    Created sign up view

---
+++ src/components/SignUp.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="sign-up">
+        <p>Let's create a new account !</p>
+        <input type="text" placeholder="Email"><br>
+        <input type="password" placeholder="Password"><br>
+        <input type="confirmPassword" placeholder="Confirm Password"><br>
+        <button>Sign Up</button>
+        <span>or go back to login.</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'signUp',
+    data: function() {
+        return {}
+    },
+    methods: {}
+}
+</script>
+
+<style>
+    .signUp {
+        margin-top: 40px;
+    }
+    input {
+        margin: 10px 0;
+        width: 20%;
+        padding: 15px;
+    }
+    button {
+        margin-top: 10px;
+        width: 10%;
+        cursor: pointer;
+    }
+    span {
+        display: block;
+        margin-top: 20px;
+        font-size: 11px;
+    }
+</style>
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Login from '@/components/Login'
+import SignUp from '@/components/SignUp'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp
     }
   ]
 })
